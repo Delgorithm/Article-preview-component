ab99a0e571ea33223a9f397fbae7b924a6616689
Delgorithm <delgorithm@gmail.com>
2023-04-16 15:09:17 +0200

    V1.4

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const iconShare = document.querySelector('.icon-share-btn');
 
+const previewFooter = document.querySelector('.article-preview-footer');
+
 const previewVisible = document.querySelector('.article-preview-footer__visible');
 const previewHidden = document.querySelector('.article-preview-footer__hidden');
 
+const toggleClicked = "";
+
 iconShare.addEventListener('click', () => {
-    if (iconShare.toggle('clicked')) {
-        previewVisible.style.display = "none";
-        previewHidden.style.display = "flex";
-        
-    } else {
+    previewVisible.style.display = "none";
+    previewHidden.style.display = "flex";
+    previewFooter.style.backgroundColor = "black";
+    toggleClicked === true;
+    }
+);
+
+iconShare.addEventListener('click', () => {
+    if (toggleClicked === true) {
         previewVisible.style.display = "block";
         previewHidden.style.display = "none";
     }
